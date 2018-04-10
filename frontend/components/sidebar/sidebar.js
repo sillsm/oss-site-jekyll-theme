fff3ada3e3b0fb0e11535906a85819d6d0d860d6
Denis.Chernov <d9onis@gmail.com>
2018-04-10 17:33:01 +0300

    feat(UI): update layouts

--- frontend/components/sidebar/sidebar.js
+++ frontend/components/sidebar/sidebar.js
@@ -1,10 +1,6 @@
 import './sidebar.scss';
 import $ from 'jquery';
-import Accordion from 'accordion/dist/common-js/accordion';
 
 $(document).ready(() => {
-  const el = document.querySelector(".accordion");
 
-
-  new Accordion(el);
 });
