422a7bc5810c732491e6abb1c9f2776d7fe51cd8
kisenka <qtuzov@gmail.com>
2018-04-12 17:57:03 +0300

    build: setup local preview with only-Docker option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+cd /code
+rm -rf ./demo-site/_site/
 bundle install --local
-rake preview
+
+if [ "$JEKYLL_ENV" == "development" ]; then
+  rake preview
+elif [ "$JEKYLL_ENV" == "preview" ]; then
+  cd /code/demo-site
+  bundle exec jekyll serve --watch --force_polling --host 0.0.0.0
+elif [ "$JEKYLL_ENV" == "build" ]; then
+  cd /code/demo-site
+  bundle exec jekyll build
+fi
