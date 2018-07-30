d4def02823f499e729d0967238e3f1b617099339
Max <sillsm@gmail.com>
2018-07-30 14:36:07 -0700

    First cut

--- frontend/components/logos/logos.js
+++
@@ -1,5 +0,0 @@
-// Logos import
-import '@jetbrains/logos/jetbrains/apple-mask-icon.svg';
-import '@jetbrains/logos/jetbrains/jetbrains-simple.svg';
-import '@jetbrains/logos/jetbrains/jetbrains-variant-4.svg';
-require.context('@jetbrains/logos/jetbrains', false, /\.(png|ico)$/);
--- frontend/components/serach-box/serach-box.js
+++
@@ -1,1 +0,0 @@
-import './serach-box.scss';
